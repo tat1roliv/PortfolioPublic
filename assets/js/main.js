5dce0a54c5d01ea137d935615e897d3e25bd529a
tati oliveira <tat1roliv@gmail.com>
2021-04-20 17:32:13 -0300

    :up: js main file

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,32 +1,18 @@
-/*
- circle-chart for languages
- */
- $(function(){
-  $('.circlechart').circlechart();
 
+ $(function(){
+  $('.circlechart').circlechart(); /*circle-chart for languages [jquery]*/
+  $('[data-toggle="tooltip"]').tooltip();/*tooltip [bootstrap]*/
+  menuHamburguer();
 });
 
-/*
- tooltip
- */
- $(function(){
-  $('[data-toggle="tooltip"]').tooltip();
-
-});
 
 /*hamburguer*/
-let hamburguer = document.querySelector(".hamburguer");
-
-hamburguer.addEventListener("click", () => {
+function menuHamburguer(){
+    let hamburguer = document.querySelector(".hamburguer");
+    hamburguer.addEventListener("click", () => {
     document.querySelector(".container").classList.toggle("show-menu");
 });
+}
 
-document.querySelector("#qtPages").addEventListener("change", updatePrice);
-document.querySelector("#js").addEventListener("change", updatePrice);
-document.querySelector("#layout-yes").addEventListener("change", updatePrice);
-document.querySelector("#layout-not").addEventListener("change", updatePrice);
-document.querySelector("#deadline").addEventListener("change", function(){
-    const deadlineUser = document.querySelector("#deadline").value;
-    document.querySelector("label[for=deadline]").innerHTML = `Deadline in ${deadlineUser} weeks.`;
-    updatePrice();
-});
+
+
