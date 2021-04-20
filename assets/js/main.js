e9320b1c2e787011500f0d985699317d64e2e70d
tati oliveira <tat1roliv@gmail.com>
2021-04-20 16:15:50 -0300

    :up: tooltip

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,6 +6,13 @@
 
 });
 
+/*
+ tooltip
+ */
+ $(function(){
+  $('[data-toggle="tooltip"]').tooltip();
+
+});
 
 /*hamburguer*/
 let hamburguer = document.querySelector(".hamburguer");
@@ -23,47 +30,3 @@ document.querySelector("#deadline").addEventListener("change", function(){
     document.querySelector("label[for=deadline]").innerHTML = `Deadline in ${deadlineUser} weeks.`;
     updatePrice();
 });
-
-/*
-Pricing Policy
-Page - U$20 | Script - 10% per Page |Layout - U$100 | Emergency - 10% total per week
-*/
-
-function updatePrice() {
-    const numberPages = document.querySelector("#qtPages").value;
-    const needScript = document.querySelector("#js").checked;
-    const needLayout = document.querySelector("#layout-yes").checked;
-    const deadlineUser = document.querySelector("#deadline").value;
-    
-    let price = numberPages*20;
-    if (needScript) price *= 1.1;
-    if(needLayout) price += 100;
-    let needEmergency = 1 - deadlineUser * 0.1;
-    price *= 1 + needEmergency;
-
-    document.querySelector("#price").innerHTML = `US$ ${price.toFixed(2)}`;
-}
-
-
-
-/*
-$().ready(function() {
-	setTimeout(function () {
-		$('.english').hide(); 
-	}, 2500); 
-});
-*/
-
-/*
-$().ready(function() {
-	setTimeout(function() {
-        $('.english').fadeIn( 500 ).delay( 1500 ).fadeOut( 1000 );
-      }, 2500); 
-      setTimeout(function() {
-        $('.spanish').fadeIn( 500 ).delay( 1500 ).fadeOut( 1000 );
-      }, 2500); 
-      setTimeout(function() {
-        $('.portuguese').fadeIn( 500 ).delay( 1500 ).fadeOut( 1000 );
-      }, 2500);   
-});
-*/
